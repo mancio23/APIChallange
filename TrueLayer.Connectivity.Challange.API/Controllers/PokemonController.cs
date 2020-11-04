9b876bddfc2bbccb798ed311dcb60e9e8d895798
Roberto Mancinelli <roberto.mancinelli.ing@gmail.com>
2020-11-04 11:25:53 +0100

    Add project files.

---
+++ TrueLayer.Connectivity.Challange.API/Controllers/PokemonController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace TrueLayer.Connectivity.Challange.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PokemonController : ControllerBase
+    {
+        private ILogger<string> _logger;
+
+        public PokemonController(ILogger<string> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return string.Empty;
+        }
+    }
+}
